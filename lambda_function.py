241689870001d3bbeeeada3d33154a5de72bc372
“Thomas <Thomas.Nakaki@chromeriver.com>
2019-12-06 08:53:53 -0800

    teest

--- lambda_function.py
+++ lambda_function.py
@@ -4,19 +4,20 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def lambda_handler(event, context):
-    pd.set_option('display.max_rows', 500)
-    pd.set_option('display.max_columns', 500)
-    pd.set_option('display.width', 1000)
+  pd.set_option('display.max_rows', 500)
+  pd.set_option('display.max_columns', 500)
+  pd.set_option('display.width', 1000)
 
-    client = boto3.client('s3')
-    path = 's3://chromeriver-navigator-int-c4-dev/100SalesRecords.csv'
-    df = pd.read_csv(path)
-    print (df[:100])
+  client = boto3.client('s3')
+  path = 's3://chromeriver-navigator-int-c4-dev/100SalesRecords.csv'
+  df = pd.read_csv(path)
+  print (df[:100])
 
-    engine = create_engine('mysql+pymysql://Test_User:Test_Pasword@c4-dev-navigatordb-0.cemtanyak0jn.us-west-2.rds.amazonaws.com/chrome_navigator')
+  engine = create_engine('mysql+pymysql://cr_tester:ch2goAvaTWQa@c4-dev-navigatordb-0.cemtanyak0jn.us-west-2.rds.amazonaws.com/chrome_navigator')
 
-    with engine.connect() as conn, conn.begin():
-    #df.to_sql('sales102',conn, if_exists='append', index=False)
-    return {
+  #with engine.connect() as conn, conn.begin():
+  #df.to_sql('sales102',conn, if_exists='append', index=False)
+
+  return {
         'statusCode': 200,
-    }
+  }
